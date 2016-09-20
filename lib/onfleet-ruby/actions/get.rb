f39bd0a3f9e14f1dacdf09f8b2451944f804be12
Ramkumar R Rao <ramkumarceg@gmail.com>
2016-09-20 11:29:50 -0700

    add params to get

--- lib/onfleet-ruby/actions/get.rb
+++ lib/onfleet-ruby/actions/get.rb
@@ -2,8 +2,8 @@ module Onfleet
   module Actions
     module Get
       module ClassMethods
-        def get id
-          api_url = "#{self.api_url}/#{id}"
+        def get id, params={}
+          api_url = "#{self.api_url}/#{id}{params.to_query}"
           response  = Onfleet.request(api_url, :get)
           Util.constantize("#{self}").new(response)
         end
